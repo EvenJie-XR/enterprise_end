ab9abc562d03f3e23020288349d80a04a521bbe5
zwj <1927773824@qq.com>
2024-01-24 17:33:02 +0800

    feat: 完成工作台基本页面UI

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { createApp } from 'vue'
-import './style.scss'
 import App from './App.vue'
 import { router } from './routers'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import "virtual:svg-icons-register";
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import './style.scss'
 
 const app = createApp(App);
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
